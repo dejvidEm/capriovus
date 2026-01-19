0c2c3d31260bfe20dd1680559b9988dd69921afe
dejvidEm <bithug10@gmail.com>
2026-01-19 14:30:06 +0100

    fix

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { ArrowLeft, FileDown, X, ChevronLeft, ChevronRight } from 'lucide-react';
 import { motion } from 'framer-motion';
